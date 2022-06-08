3df1ac3d1f5be947968c230381d4ea3b77784c6f
psavarmattas <puranjaysavarmattas@gmail.com>
2022-06-08 11:34:26 +0530

    Natural Number Print

---
+++ IndustrialTrainingProjects/NaturalNoPrint.java
@@ -0,0 +1,12 @@
+// Program to print Natural numbers from 1 to 10.
+
+package IndustrialTrainingProjects;
+
+public class NaturalNoPrint {
+        public static void main(String[] args) {
+            for(int i = 1; i <= 10; i++)
+            {
+                System.out.println(i);
+            }
+        }
+}
